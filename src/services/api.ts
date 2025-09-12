19fd5014340c5fb0f170c479ffe85170c0756109
Dev-Ramos <lucaxiDev@gmail.com>
2025-09-12 07:45:48 -0300

    fix: guardar a baseUrl de forma protegida.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://beta.guardia-api.box3.work/api"
+  baseURL: process.env.BASE_URL 
 })
-
+// Toda vez que uma requisição é feita os interceptors verificam
+// se há algum token salvo no localStorage e usa-o na requisição
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token')
   if (token) {
