9d12b1ba3dc30a77124a515299763b66c39a9766
Dev-Ramos <lucaxiDev@gmail.com>
2025-09-12 09:41:46 -0300

    fix: ajustar a importação da variavel vite_base_url para a api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.BASE_URL 
+  baseURL: import.meta.env.VITE_BASE_URL 
 })
+
 // Toda vez que uma requisição é feita os interceptors verificam
 // se há algum token salvo no localStorage e usa-o na requisição
 api.interceptors.request.use(config => {
